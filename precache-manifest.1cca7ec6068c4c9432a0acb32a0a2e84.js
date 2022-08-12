d1adf5d3274a62c3c142f7299e01d65d43cf11db
qutub-bw <qutubuddin70@gmail.com>
2022-08-12 22:58:29 +0400

    Updates

---
+++ precache-manifest.1cca7ec6068c4c9432a0acb32a0a2e84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dd65cc52ed90931a198",
+    "url": "/TextUtils/static/css/main.a8e3008e.chunk.css"
+  },
+  {
+    "revision": "6dd65cc52ed90931a198",
+    "url": "/TextUtils/static/js/main.6dd65cc5.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/TextUtils/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "a12dbe3b84df6eb43734",
+    "url": "/TextUtils/static/js/2.a12dbe3b.chunk.js"
+  },
+  {
+    "revision": "94fa866583836ad85178",
+    "url": "/TextUtils/static/js/runtime~main.94fa8665.js"
+  },
+  {
+    "revision": "0f9977d7d97efd5fd536ec9ad5964c3f",
+    "url": "/TextUtils/index.html"
+  }
+];
